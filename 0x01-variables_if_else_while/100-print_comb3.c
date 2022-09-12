221e025a0c5a5a4d2f19712929b557698c4c868b
Nono0007 <noreenmagaya24@gmail.com>
2022-09-12 11:42:42 -0700

    task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - program that prints all possible different combinations of 2 digits
+* Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+	while (c <= 99)
