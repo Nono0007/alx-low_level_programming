cf5b746d2ce09e975a7773bcfdf05454f2c998b9
Nono0007 <noreenmagaya24@gmail.com>
2022-09-13 11:14:40 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
- */
+* print_alphabet - function to print abc
+*
+* Return: 0
+*/
 void print_alphabet(void)
 {
 	char c;
@@ -13,4 +13,4 @@ void print_alphabet(void)
 		_putchar(c);
 	}
 _putchar('\n');
-}	
+}
