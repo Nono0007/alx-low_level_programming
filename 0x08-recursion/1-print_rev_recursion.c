032332e98116aa7e73c01f1dbcec740b5cd0ead8
Nono0007 <noreenmagaya24@gmail.com>
2022-09-28 05:47:41 -0700

    task 1 & 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
+ *
+ * Return: always 0
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
