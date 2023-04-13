b0ffef7655bfa931c6beb7b0bdd67b5c74e64a86
Nono0007 <noreenmagaya24@gmail.com>
2023-04-13 14:26:52 -0700

    fixing error task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,17 +2,18 @@
 #include <math.h>
 
 /**
- * jump_search - searches for a value in an array of,
- * integers using the Jump earch algorithm
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
  * @array: input array
  * @size: size of the array
- * @value: value to search im
+ * @value: value to search in
  * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
 	int index, m, k, prev;
+
 	if (array == NULL || size == 0)
 		return (-1);
 
